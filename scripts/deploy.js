55c99d47b03b85c671d61b685bd8051e2511c417
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-12-04 17:06:05 -0500

    Clean up hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  // We get the contract to deploy
-  // const Greeter = await hre.ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
-  // await greeter.deployed();
-
   const RPS = await hre.ethers.getContractFactory("RPS");
   const rps = await RPS.deploy();
   await rps.deployed();
-
-  // console.log("Greeter deployed to:", greeter.address);
   console.log("RPS deployed to:", rps.address);
 }
 
