e5639bd62862dc9e2ea1a2348ab468f9a54a0b15
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-12-04 22:52:27 -0500

    Add Alert Dialog

--- src/components/Buttons/CancelButton.js
+++ src/components/Buttons/CancelButton.js
@@ -1,23 +1,76 @@
 import { Button } from "@chakra-ui/button";
 import React, { useContext } from "react";
 import AppContext from "../../utils/AppContext";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
+import { Text } from "@chakra-ui/layout";
 
 export const CancelButton = ({ cancel }) => {
   const value = useContext(AppContext);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
   return (
-    <Button
-      disabled={
-        value.state.status === "Cancellation in progress..." ||
-        value.state.status === "Game finished! GG!" ||
-        value.state.status === "Waiting for your commitment..." ||
-        value.state.status === "Game cancelled!"
-      }
-      onClick={() => {
-        cancel();
-      }}
-      colorScheme="red"
-    >
-      Cancel
-    </Button>
+    <div>
+      <Button
+        disabled={
+          value.state.status === "Cancellation in progress..." ||
+          value.state.status === "Game finished! GG!" ||
+          value.state.status === "Waiting for your commitment..." ||
+          value.state.status === "Game cancelled!" ||
+          value.state.status === "Sending commitment..."
+        }
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        colorScheme="red"
+      >
+        Cancel
+      </Button>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent top="6rem">
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Cancel This Game
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              <Text>
+                Are you sure? Both players will be refunded their bets, but{" "}
+                <Text as="span" color="tomato" fontWeight="bold">
+                  this game will be lost forever.
+                </Text>{" "}
+              </Text>
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  cancel();
+                  onClose();
+                }}
+                mr={3}
+              >
+                Confirm
+              </Button>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </div>
   );
 };
--- src/components/Buttons/ReplayButton.js
+++ src/components/Buttons/ReplayButton.js
@@ -1,11 +1,23 @@
 import { Button } from "@chakra-ui/button";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
 import React, { useContext } from "react";
 import AppContext from "../../utils/AppContext";
 import { nanoid } from "nanoid";
 import { ethers } from "ethers";
+import { Text } from "@chakra-ui/layout";
 
 export const ReplayButton = () => {
   const value = useContext(AppContext);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
   const reset = () => {
     const newId = nanoid();
     value.setGameId(newId);
@@ -17,18 +29,61 @@ export const ReplayButton = () => {
     value.setDisableCancel(false);
   };
   return (
-    <Button
-      colorScheme="teal"
-      onClick={reset}
-      disabled={
-        value.state.status === "Waiting for your commitment..." ||
-        value.state.status === "Waiting for opponent's commitment..." ||
-        value.state.status === "Cancellation in progress..." ||
-        value.state.status === "Waiting for your verification..." ||
-        value.state.status === "Waiting for opponent's verification..."
-      }
-    >
-      Replay
-    </Button>
+    <>
+      <Button
+        colorScheme="teal"
+        onClick={() => setIsOpen(true)}
+        // disabled={
+        //   value.state.status === "Waiting for your commitment..." ||
+        //   value.state.status === "Waiting for opponent's commitment..." ||
+        //   value.state.status === "Cancellation in progress..." ||
+        //   value.state.status === "Waiting for your verification..." ||
+        //   value.state.status === "Waiting for opponent's verification..." ||
+        //   value.state.status === "Sending commitment..."
+        // }
+      >
+        Replay
+      </Button>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent top="6rem">
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Play Another Game
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              <Text>
+                Are you sure?{" "}
+                <Text as="span" color="tomato" fontWeight="bold">
+                  Your bets will NOT be refunded if you didn{"'"}t cancel the
+                  game.
+                </Text>{" "}
+                If you begin another game, this game will be lost forever.
+              </Text>
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  reset();
+                  onClose();
+                }}
+                mr={3}
+              >
+                Confirm
+              </Button>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
